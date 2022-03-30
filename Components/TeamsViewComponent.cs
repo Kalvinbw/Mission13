0f558b0bded3bd08474d0f6470fa49b8af86e2e2
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-03-30 12:08:39 -0600

    finish mission13

--- Components/TeamsViewComponent.cs
+++ Components/TeamsViewComponent.cs
@@ -16,10 +16,19 @@ namespace Mission13.Components
 
         public IViewComponentResult Invoke()
         {
+            var id = RouteData?.Values["teamID"];
             ViewBag.SelectedType = RouteData?.Values["teamID"];
 
             var teams = _repo.Teams
                 .OrderBy(x => x.TeamName);
+
+            var team = teams.FirstOrDefault(x => x.TeamID.ToString() == id);                                        
+
+            if (team != null)
+            {
+                ViewBag.SelectedTeam = team.TeamName;
+            }
+
 
             return View(teams);
         }
